d24cfccfb0c96750fa2c0614f042bc3d9e167689
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-07 04:25:23 +0800

    Add prompt to get user input

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const container = document.querySelector(".container");
+const sizeBtn = document.querySelector(".size-btn");
 
-// let i = container.childNodes.length;
+let squareX = 0;
+let totalSquare = 0;
+
+sizeBtn.addEventListener("click", () => {
+  squareX = prompt("Number of squares");
+  totalSquare = squareX * squareX;
+  console.log(totalSquare);
+  // re render the squares
+});
 
 for (let i = 0; i < 100; i++) {
   const square = document.createElement("div");
@@ -15,6 +24,7 @@ for (let i = 0; i < 100; i++) {
 }
 
 const pixels = document.querySelectorAll(".pixel");
+// pixels.style.flex = "1";
 
 pixels.forEach((pixel) => {
   pixel.addEventListener("mouseover", () => {
